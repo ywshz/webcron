51802cc671f7b6c6573c31bf17dcec743abfe800
hzyangwangshu <hzyangwangshu@corp.netease.com>
2016-11-09 09:44:15 +0800

    remobe code

---
+++ run.bat
@@ -0,0 +1,24 @@
+case $1 in
+	start)
+		nohup ./webcron.exe 2>&1 >> info.log 2>&1 /dev/null &
+		echo "服务已启动..."
+		sleep 1
+	;;
+	stop)
+		killall webcron.exe
+		echo "服务已停止..."
+		sleep 1
+	;;
+	restart)
+		killall webcron
+		sleep 1
+		nohup ./webcron.exe 2>&1 >> info.log 2>&1 /dev/null &
+		echo "服务已重启..."
+		sleep 1
+	;;
+	*) 
+		echo "$0 {start|stop|restart}"
+		exit 4
+	;;
+esac
+
